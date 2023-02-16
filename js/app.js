0b0000903714eae92a06aec7b07e2c2002460e2a
Yusuf Sezer <yusufsezer@mail.com>
2023-02-16 23:36:43 +0300

    ticks elements creation changed

--- js/app.js
+++ js/app.js
@@ -1,8 +1,20 @@
-var time = new Date(),
-    hours = time.getHours(),
-    minutes = time.getMinutes(),
+var time = {},
+    hours = 0,
+    minutes = 0,
+    seconds = 0;
+
+function init() {
+    time = new Date();
+    hours = time.getHours();
+    minutes = time.getMinutes();
     seconds = time.getSeconds();
 
-document.getElementById("second").className = "second second-" + seconds;
-document.getElementById("minute").className = "minute minute-" + minutes;
-document.getElementById("hour").className = "hour hour-" + hours;
+    var tickSpan = '<span></span>'.repeat(60);
+    document.getElementsByClassName('ticks')[0].innerHTML = tickSpan;
+
+    document.getElementById("second").className = "second second-" + seconds;
+    document.getElementById("minute").className = "minute minute-" + minutes;
+    document.getElementById("hour").className = "hour hour-" + hours;
+}
+
+window.addEventListener('DOMContentLoaded', init);
